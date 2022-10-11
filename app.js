474eb1389d7d81e3cba68dfd6bf6679a6592005f
Grey <58967769+FlameRender@users.noreply.github.com>
2022-10-10 22:45:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require("body-parser");
 const UsersUtil = require("./utils/UserUtil");
 const app = express();
 
+const { PORT = 3000 } = process.env;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("static"));
