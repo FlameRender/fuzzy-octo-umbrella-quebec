680bc9013ff49cbedf903b69a1182cfec134aa3f
Greyson Holmes <greyson.holmes@yahoo.com>
2022-10-10 21:49:55 -0400

    big update

---
+++ app.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const UsersUtil = require("./utils/UserUtil");
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("static"));
+app.set("view engine", "ejs");
+
+//initial reply
+app.get("/", async (req, res) => {
+   something = await UsersUtil.readDB();
+  
+    res.render("index", {
+      something: something,
+    });
+  });
+// creation call
+app.post("/submit", async (req, res) => {
+    const response = await UsersUtil.create(req.body);
+  
+    if (!response) console.error(response);
+  
+    res.redirect("/");
+  });
+
+//deletion
+app.post("/delete", async (req, res) => {
+    const response = await UsersUtil.deleteEntry(req.body);
+  
+    if (!response) console.error(response);
+  
+    res.redirect("/");
+  });
+
+//Listening
+app.listen(PORT, () =>
+  console.log(`Server is running: http://localhost:${PORT}/`)
+);
---
+++ utils/UserUtil.js
@@ -0,0 +1,60 @@
+const { query } = require("express");
+const { ObjectId } = require("mongodb");
+const client = require("../db/client");
+
+module.exports = class UsersUtil {
+  static async create(userData) {
+    try {
+      const collection = client.db("NotesDB").collection("Quebec");
+
+      const doc = {
+        content: userData
+      }
+
+      const response = await collection.insertOne(userData);
+
+      if (!response) throw new Error("Error saving user");
+
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+
+  static async readDB() {
+    try {
+      const collection = client.db("NotesDB").collection("Quebec");
+
+      const something = collection.find().limit(1).sort({$natural:-1}) 
+
+      return something;
+    } catch (err) {
+      console.error("Nothing Found");
+      return "No Entries";
+    }
+  }
+
+  static async deleteEntry(userData) {
+    try {
+      // Connect to a collection using the client connection object created in ../db/client.js
+      const collection = client.db("NotesDB").collection("Quebec");
+
+      // Use this collection to delete one user if the _id in Mongo matches the userId from request
+      const response = await collection.deleteOne({
+        Random: ObjectId(userData),
+      });
+      // Throw an error if there is no response
+      if (!response) throw new Error("Error deleting user");
+
+      // Return true if succeeded
+      return true;
+    } catch (error) {
+      // Error thrown above will be shown here
+      console.error(error);
+
+      // Return false if failed
+      return false;
+    }
+  }
+};
