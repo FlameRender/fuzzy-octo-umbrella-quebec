d0d3bc6e06fb42c11501d399fa8c10d802fa9be4
Grey <58967769+FlameRender@users.noreply.github.com>
2022-10-13 15:44:53 -0400

    Update UserUtil.js

--- utils/UserUtil.js
+++ utils/UserUtil.js
@@ -4,14 +4,15 @@ const { ObjectId } = require("mongodb");
 //Reference to MongoDB connection at ../db/client
 const client = require("../db/client");
 
+ //conection to db and specific collection
+ const collection = client.db("NotesDB").collection("Quebec");
+
+
 
 //UserUtil Reference HERE
 module.exports = class UsersUtil {
   static async create(userData) {
     try {
-      //conection to db and specific collection
-      const collection = client.db("NotesDB").collection("Quebec");
-
       //inserts userData
       const response = await collection.insertOne(userData);
 
@@ -26,8 +27,6 @@ module.exports = class UsersUtil {
 
   static async readDB() {
     try {
-      //conection to db and specific collection
-      const collection = client.db("NotesDB").collection("Quebec");
       //.find used with.sortto find last known _id: then convcerted to array
       something = await collection.find().sort({_id: -1}).toArray();
       //returns last entry in array returned
@@ -41,8 +40,6 @@ module.exports = class UsersUtil {
 
   static async updateOne(userData) {
     try {
-      //conection to db and specific collection
-      const collection = client.db("NotesDB").collection("Quebec");
       //update one using passed data
       const response = await collection.updateOne({Random: userData.Old}, {$set:{Random: userData.New}});
 
@@ -57,9 +54,6 @@ module.exports = class UsersUtil {
 
   static async deleteEntry(userData) {
     try {
-      // Connect to a collection using the client connection object created in ../db/client.js
-      const collection = client.db("NotesDB").collection("Quebec");
-
       // Use this collection to delete one user if the Random: in Mongo matches the userId from request
       const response = await collection.deleteOne({Random: userData.Random});
       // Throw an error if there is no response
